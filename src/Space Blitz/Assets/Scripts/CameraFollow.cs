d4e0f339e810aa6fee8d9d6f562124849be6fd01
xyzch001 <595389239@qq.com>
2018-10-27 16:44:18 -0400

    Camera Follow

---
+++ src/Space Blitz/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject cameraFather;
+    public Transform target;
+    public float rotatespeed;
+    public float scalespeed;
+    public float min;
+    public float max;
+    public Transform obj;
+    public float cameraSpeed;
+
+    private bool isLock = false;
+    private Vector3 fCurrentRotation;
+    private float currentSacale;
+    private Vector3 currentRotation;
+    private Vector3 lastMousePosition;
+    // Use this for initialization
+    void Start()
+    {
+        fCurrentRotation = cameraFather.transform.eulerAngles;
+        currentRotation = transform.eulerAngles;
+        lastMousePosition = Input.mousePosition;
+        cameraFather.transform.position = obj.position + new Vector3(0, 5, -50);
+        cameraFather.transform.rotation = obj.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj != null && Input.GetKeyDown(KeyCode.Space) && isLock == false)
+        {
+            isLock = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isLock = false;
+        }
+        if (isLock)
+        {
+            cameraFather.transform.position = obj.position + new Vector3(0, 5, -50);
+            cameraFather.transform.eulerAngles = obj.eulerAngles;
+            fCurrentRotation = new Vector3(0.0f, 0.0f, 0.0f);
+            currentRotation = fCurrentRotation;
+        }
+        else if (!Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= Screen.width - 60)
+            {
+                cameraFather.transform.Translate(new Vector3(1, 0, 0) * cameraSpeed * Time.deltaTime);
+            }
+            else if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= 60)
+            {
+                cameraFather.transform.Translate(new Vector3(1, 0, 0) * -cameraSpeed * Time.deltaTime);
+            }
+            if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 60)
+            {
+                cameraFather.transform.Translate(new Vector3(0, 0, 1) * -cameraSpeed * Time.deltaTime);
+            }
+            else if (Input.mousePosition.y >= Screen.height - 60 && Input.mousePosition.y <= Screen.height)
+            {
+                cameraFather.transform.Translate(new Vector3(0, 0, 1) * cameraSpeed * Time.deltaTime);
+            }
+        }
+
+        
+        currentSacale = 0;
+        //transform.LookAt (camerachild.position);
+        Vector3 mouseDelta = Input.mousePosition - lastMousePosition;
+        lastMousePosition = Input.mousePosition;
+        if (Input.GetMouseButton(0))
+        {
+            fCurrentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
+            currentRotation.x += mouseDelta.y * rotatespeed * Time.deltaTime;
+            currentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
+        }
+        currentSacale = Input.mouseScrollDelta.y * scalespeed * Time.deltaTime;
+        if (currentSacale != 0)
+            currentSacale = Mathf.Clamp(currentSacale, min, max);
+        Debug.Log(currentSacale);
+        transform.position = target.position;
+        cameraFather.transform.eulerAngles = fCurrentRotation;
+        transform.eulerAngles = currentRotation;
+        transform.Translate(new Vector3(0, 0, currentSacale));
+    }
+}
