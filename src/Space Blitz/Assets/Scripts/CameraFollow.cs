9a6aae7818125c322944e3abccef081b4ca1d1ce
Weihao Yan (RIT Student) <wy2751@ad.rit.edu>
2018-10-29 21:29:36 -0400

    Modification of Movement_And_Camera

--- src/Space Blitz/Assets/Scripts/CameraFollow.cs
+++ src/Space Blitz/Assets/Scripts/CameraFollow.cs
@@ -14,41 +14,97 @@ public class CameraFollow : MonoBehaviour
     public float max;
     public Transform obj;
     public float cameraSpeed;
+    public float distance = 50f;
+    public float height = 10f;
+    float rotationDamping = 3f;
 
     private bool isLock = false;
     private Vector3 fCurrentRotation;
     private float currentSacale;
     private Vector3 currentRotation;
     private Vector3 lastMousePosition;
+
+    public float desiredZRotationangle;
+    public float currentZRotationangle;
+    public float desiredXRotationAngle;
+    public float desiredYRotationAngle;
+
     // Use this for initialization
     void Start()
     {
-        fCurrentRotation = cameraFather.transform.eulerAngles;
-        currentRotation = transform.eulerAngles;
+        //currentRotation = transform.eulerAngles;
         lastMousePosition = Input.mousePosition;
-        cameraFather.transform.position = obj.position + new Vector3(0, 5, -50);
-        cameraFather.transform.rotation = obj.rotation;
+        //cameraFather.transform.position = obj.position + new Vector3(0, 5, -50);
+        //cameraFather.transform.rotation = obj.rotation;
+
+        desiredZRotationangle = obj.eulerAngles.z;
+        currentZRotationangle = transform.eulerAngles.z;
+        desiredXRotationAngle = obj.eulerAngles.x;
+        desiredYRotationAngle = obj.eulerAngles.y;
+
+        currentRotation.y = transform.eulerAngles.y;
+
+        
+        currentRotation.x = transform.eulerAngles.x;
+
+
+        lastMousePosition = Input.mousePosition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (obj != null && Input.GetKeyDown(KeyCode.Space) && isLock == false)
-        {
-            isLock = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space))
+        changeView();
+    }
+
+
+
+    void LateUpdate()
+    {
+        
+    }
+
+    private void changeView()
+    {
+        
+
+
+        if (obj != null && Input.GetMouseButton(0) )
         {
             isLock = false;
         }
+        else if (!Input.GetMouseButton(0))
+        {
+            isLock = true;
+        }
+
+
         if (isLock)
         {
-            cameraFather.transform.position = obj.position + new Vector3(0, 5, -50);
-            cameraFather.transform.eulerAngles = obj.eulerAngles;
-            fCurrentRotation = new Vector3(0.0f, 0.0f, 0.0f);
-            currentRotation = fCurrentRotation;
+            camera_Follow();
+            currentRotation.x = obj.eulerAngles.x;
+            currentRotation.y = obj.eulerAngles.y;
+            currentRotation.z = obj.eulerAngles.z;
         }
-        else if (!Input.GetMouseButton(0))
+        else
+        {
+            camera_Rotate();
+
+            
+            
+            /*transform.eulerAngles = obj.eulerAngles;
+
+            currentRotation.x += mouseDelta.y * rotatespeed * Time.deltaTime;
+            currentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
+
+            currentRotation = Quaternion.Euler(currentRotation.x, currentRotation.y, currentZRotationangle);
+
+            transform.eulerAngles.x = currentRotation.x;*/
+
+
+        }
+        /*else if (!Input.GetMouseButton(0))
         {
             if (Input.mousePosition.x <= Screen.width && Input.mousePosition.x >= Screen.width - 60)
             {
@@ -66,26 +122,93 @@ public class CameraFollow : MonoBehaviour
             {
                 cameraFather.transform.Translate(new Vector3(0, 0, 1) * cameraSpeed * Time.deltaTime);
             }
-        }
+        }*/
+
+
+
+        //transform.LookAt (camerachild.position);
 
         
-        currentSacale = 0;
-        //transform.LookAt (camerachild.position);
+        //transform.position = target.position;
+        //cameraFather.transform.eulerAngles = fCurrentRotation;
+        //transform.eulerAngles = currentRotation;
+
+    }
+
+    private void camera_Rotate()
+    {
         Vector3 mouseDelta = Input.mousePosition - lastMousePosition;
         lastMousePosition = Input.mousePosition;
-        if (Input.GetMouseButton(0))
-        {
-            fCurrentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
-            currentRotation.x += mouseDelta.y * rotatespeed * Time.deltaTime;
-            currentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
-        }
+
+        if (!obj)
+            return;
+
+        float desiredYRotationAngle = obj.eulerAngles.y;
+        float currentYRotationAngle = transform.eulerAngles.y;
+
+        float desiredXRotationAngle = obj.eulerAngles.x;
+        float currentXRotationAngle = transform.eulerAngles.x;
+
+        float desiredZRotationangle = obj.eulerAngles.z;
+        float currentZRotationangle = transform.eulerAngles.z;
+
+        currentYRotationAngle = Mathf.LerpAngle(currentYRotationAngle, desiredYRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentXRotationAngle = Mathf.LerpAngle(currentXRotationAngle, desiredXRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentZRotationangle = Mathf.LerpAngle(currentZRotationangle, desiredZRotationangle, rotationDamping * Time.deltaTime);
+
+        Quaternion currentRotation1 =  Quaternion.Euler(currentXRotationAngle, currentYRotationAngle, currentZRotationangle);
+
+        transform.position = obj.position;
+        transform.position -= currentRotation1 * Vector3.forward * distance;
+        transform.position += transform.up * height;
+        transform.LookAt(obj, obj.up);
+        currentRotation.x += mouseDelta.y * rotatespeed * Time.deltaTime;
+        currentRotation.y += mouseDelta.x * rotatespeed * Time.deltaTime;
+
+
+        transform.eulerAngles = currentRotation;
+
+        
+    } 
+
+    private void camera_Follow()
+    {
+        if (!obj)
+            return;
+
+        float desiredYRotationAngle = obj.eulerAngles.y;
+        float currentYRotationAngle = transform.eulerAngles.y;
+
+        float desiredXRotationAngle = obj.eulerAngles.x;
+        float currentXRotationAngle = transform.eulerAngles.x;
+
+        float desiredZRotationangle = obj.eulerAngles.z;
+        float currentZRotationangle = transform.eulerAngles.z;
+
+        currentYRotationAngle = Mathf.LerpAngle(currentYRotationAngle, desiredYRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentXRotationAngle = Mathf.LerpAngle(currentXRotationAngle, desiredXRotationAngle, rotationDamping * Time.deltaTime);
+
+        currentZRotationangle = Mathf.LerpAngle(currentZRotationangle, desiredZRotationangle, rotationDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(currentXRotationAngle, currentYRotationAngle, currentZRotationangle);
+
+        currentSacale = 0;
         currentSacale = Input.mouseScrollDelta.y * scalespeed * Time.deltaTime;
         if (currentSacale != 0)
             currentSacale = Mathf.Clamp(currentSacale, min, max);
         Debug.Log(currentSacale);
-        transform.position = target.position;
-        cameraFather.transform.eulerAngles = fCurrentRotation;
-        transform.eulerAngles = currentRotation;
-        transform.Translate(new Vector3(0, 0, currentSacale));
+        
+
+        transform.position = obj.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        transform.LookAt(obj, obj.up);
+
+        transform.position += transform.up * height;
+        
     }
+
 }
---
+++ src/Space Blitz/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    float maxVelocity = 300f;
+    float minVelocity = -50f;
+    public float shipVelocity = 0f;
+    Rigidbody rb;
+    // Use this for initialization
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * shipVelocity;
+        switch (m_input.Mode)
+        {
+            case Ship.Light:
+                LightShipMovement();
+                break;
+
+            case Ship.Medium:
+                MediumShipMovement();
+                break;
+
+            case Ship.Heavy:
+                HeavyShipMovement();
+                break;
+
+        }
+    }
+
+   
+
+    [SerializeField, Tooltip("ShipClass options.")]
+    private InputSettings m_input = new InputSettings
+    {
+        Mode = Ship.Light
+    };
+
+    [Serializable]
+    private struct InputSettings
+    {
+        [Tooltip("Choose ship class.")] public Ship Mode;  
+    }
+
+
+
+
+    public enum Ship
+    {
+        Light,
+        Medium,
+        Heavy
+    }
+
+    private void LightShipMovement()
+    {
+        float lightAcceleration = 100f;
+        float lightTurnRate = 40f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (shipVelocity < maxVelocity)
+                shipVelocity += lightAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (shipVelocity > minVelocity)
+                shipVelocity -= lightAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(lightTurnRate * Time.deltaTime * (-1), 0, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(lightTurnRate * Time.deltaTime, 0, 0), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, (-2) * lightTurnRate * Time.deltaTime, 2 * lightTurnRate * Time.deltaTime), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 2 * lightTurnRate * Time.deltaTime, (-2) * lightTurnRate * Time.deltaTime), Space.Self);
+        }
+    }
+
+    private void MediumShipMovement()
+    {
+        float mediumAcceleration = 40f;
+        float mediumTurnRate = 20f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (shipVelocity < maxVelocity)
+                shipVelocity += mediumAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (shipVelocity > minVelocity)
+                shipVelocity -= mediumAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(mediumTurnRate * Time.deltaTime * (-1), 0, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(mediumTurnRate * Time.deltaTime, 0, 0), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, 0, 2 * mediumTurnRate * Time.deltaTime), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 0, (-2) * mediumTurnRate * Time.deltaTime), Space.Self);
+        }
+    }
+
+    private void HeavyShipMovement()
+    {
+        float HeavyAcceleration = 40f;
+        float HeavyTurnRate = 5f;
+        float HeavyTwistRate = 2f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (shipVelocity < maxVelocity)
+                shipVelocity += HeavyAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (shipVelocity > minVelocity)
+                shipVelocity -= HeavyAcceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(new Vector3(HeavyTurnRate * Time.deltaTime * (-1), 0, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(new Vector3(HeavyTurnRate * Time.deltaTime, 0, 0), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, (-2) * HeavyTurnRate * Time.deltaTime, 0), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 2 * HeavyTurnRate * Time.deltaTime, 0), Space.Self);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, HeavyTwistRate * Time.deltaTime), Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, (-1) * HeavyTwistRate * Time.deltaTime), Space.Self);
+        }
+    }
+}
